5cc98ee9d03ec3c7014cf3475f93220104821064
raaaaaaaaaf <rridad@ssct.edu.ph>
2023-09-05 09:24:28 +0800

    Add UI Files

---
+++ src/_mock/account.jsx
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------
+
+const account = {
+    displayName: 'Raffy Jay Ridad',
+    email: 'demo@minimals.cc',
+    photoURL: '/assets/images/avatars/avatar_default.jpg',
+  };
+  
+  export default account;
+  
